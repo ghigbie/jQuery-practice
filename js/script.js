77c43a8cf474c5865b8c5222eb1e2c6a7f9af233
ghigbie <gehigbie@gmail.com>
2018-08-26 19:12:01 -0600

    Add new fadeTo functions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-$(function() {
+// $(function() {
 
 
-  $(".red-box").fadeOut(2000);
-  setTimeout(() => {
-    $(".green-box").fadeOut(2000);
-  }, 2000);
-  $(".red-box").fadeIn(1000);
+//   $(".red-box").fadeOut(2000);
+//   setTimeout(() => {
+//     $(".green-box").fadeOut(2000);
+//   }, 2000);
+//   $(".red-box").fadeIn(1000);
+//   $(".red-box").fadeTo(2000, 0.5);
+// });
+
+$(() => {
+  $('.red-box').fadTo(1000, 0.8);
+  $('.green-box').fadeTo(2000, 0.5);
+  $('.blue-box').fadeTo(3000, 0.2);
+
 });
